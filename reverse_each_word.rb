4acdbd4ecc1dcc28294582695c11688c6d4f9a13
Peter Werner <pcw1995@yahoo.com>
2019-09-09 20:14:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,11 @@
 def reverse_each_word(string)
   array = string.split(" ")
   array.collect.with_index{ |word,index|
+    word_arr = []
     word_arr = word.split("")
     word_arr.reverse! 
     word= word_arr.join("") 
+    array[index] = word 
   } 
   array = array.join(" ")
   return array 
