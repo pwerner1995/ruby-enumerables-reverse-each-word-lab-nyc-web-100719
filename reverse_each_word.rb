64c8d12647568538ee50f9d2f167de05bbd23412
Peter Werner <pcw1995@yahoo.com>
2019-09-09 19:57:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(string)
   array = string.split(" ")
   array.each do |word|
+    word_arr = []
     word_arr = word.split("")
     word_arr.reverse 
     word= word_arr.join("") 
