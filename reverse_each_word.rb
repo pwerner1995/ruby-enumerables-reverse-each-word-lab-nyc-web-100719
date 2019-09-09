243aeb1ff81d52cbfc8eac8ab6ed2f0ee6a768de
Peter Werner <pcw1995@yahoo.com>
2019-09-09 20:09:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,12 @@
 def reverse_each_word(string)
   array = string.split(" ")
-  array.collect{ |word,index|
+  array.each_with_index do |word,index|
     word_arr = []
     word_arr = word.split("")
     word_arr.reverse! 
     word= word_arr.join("") 
     array[index] = word 
-  }
+  end 
   array = array.join(" ")
   return array 
 end 
